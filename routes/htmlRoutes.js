13969f418b5768f4586acfc68cc9f8f06629277f
teenagegrandma <rebecca.preston90@gmail.com>
2017-11-21 19:04:00 -0600

    add htmlRoutes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,23 @@
+//dependency 
+var path = require("path");
+
+//routes
+module.exports = function(app) {
+	app.get("/", function(request, result) {
+		result.sendFile(path.join(__dirname + "/../public/index.html"));
+	});
+
+	debugger;
+
+	app.get("/event", function(request, result) {
+		result.sendFile(path.join(__dirname + "/../public/event.html"));
+	});
+
+	app.get("/event-admin", function(request, result) {
+		result.sendFile(path.join(__dirname + "/../public/event-admin.html"));
+	});
+
+	app.get("/profile", function(request, result) {
+		result.sendFile(path.join(__dirname + "/../public/profile.html"));
+	});
+}
